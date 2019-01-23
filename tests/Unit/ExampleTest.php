cda0f8a031c11ca2ce02566715b9e919ceed1fd7
zhouyao-sxs <zhou.yao@email.shaxiaoseng.com>
2019-01-23 16:43:14 +0800

    init

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace tests\unit;
+
+use tests\testcase;
+use illuminate\foundation\testing\refreshdatabase;
+
+class exampletest extends testcase
+{
+    /**
+     * a basic test example.
+     *
+     * @return void
+     */
+    public function testbasictest()
+    {
+        $this->asserttrue(true);
+    }
+}
